623c5c0d0a3d193575370b2097421c1a028f14c3
ckechios <62009820+ckechios@users.noreply.github.com>
2025-02-06 19:14:37 +0000

    Formatted with pep8

--- a_app.py
+++ a_app.py
@@ -4,7 +4,7 @@ HORIZONTAL_RULE = "-" * 40
 # Refactor the following
 
 # Assigning Lists
-#              0  1  2  
+#              0  1  2
 list_sample = [1, 2, 3, "Four", 5, 6]
 #                       -3     -2  -1
 
@@ -12,28 +12,29 @@ list_sample = [1, 2, 3, "Four", 5, 6]
 print("     Item 1:", list_sample[0])
 print("     Item 4:", list_sample[3])
 print("  Item Last:", list_sample[-1])
-print("Slice items:", list_sample[2:len(list_sample)],f" array length: {len(list_sample)}")
+print("Slice items:", list_sample[2:len(
+    list_sample)], f" array length: {len(list_sample)}")
 #                              [start_index:end_index]
 # Loop through Lists
 print(" ---- Listing Items in List ----")
 for item in list_sample:
-  print(item)
+    print(item)
 print(" ---- Listing Items using range() ----")
 # for using range(start,stop,step) / range(stop)
 for ctr in range(len(list_sample)):
-  print(ctr,":", list_sample[ctr])
+    print(ctr, ":", list_sample[ctr])
 print(HORIZONTAL_RULE)
 
 email = "j@em2.com"
 
 # Dictionary : { key_1: value_1, key_2: value_2, ..... key_n: value_n }
 dictionary_sample = {
-  "id" : 123,
-  "name": "John",
-  "email" : [
-      "j@em.com",
-      email
-  ]
+    "id": 123,
+    "name": "John",
+    "email": [
+        "j@em.com",
+        email
+    ]
 }
 
 # Access dictionaries individually
@@ -46,5 +47,5 @@ print("Email 2:")
 print(HORIZONTAL_RULE)
 # Loop through dictionaries print key : value - element type
 for key in dictionary_sample:
-  print(key, ":", dictionary_sample[key])
+    print(key, ":", dictionary_sample[key])
 print("-"*20)
